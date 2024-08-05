6aeff47245fba50009d70d79f23934fab03215f5
koreansandwich <jdh4673@naver.com>
2024-08-05 14:14:42 +0900

    jdbc

--- hello-spring/src/main/java/hello/hello_spring/repository/MemoryMemberRepository.java
+++ hello-spring/src/main/java/hello/hello_spring/repository/MemoryMemberRepository.java
@@ -9,6 +9,7 @@ import java.util.Map;
 import java.util.HashMap;
 import java.util.Optional;
 
+@Repository
 public class MemoryMemberRepository implements MemberRepository {
     private static Map<Long, Member> store = new HashMap<>();
     private static long sequence = 0L;
